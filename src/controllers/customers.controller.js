4862b881fd076425bbde977ec0506f68d0bbabe8
Caio Pires <caiogodoi_92@hotmail.com>
2023-07-29 18:19:51 -0300

    create edit customer

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -50,3 +50,39 @@ export async function getCustomersById(req, res) {
     res.status(500).send(err.message)
   }
 }
+
+export async function editCustomerById(req, res) {
+  const { id } = req.params
+  const { name, phone, cpf, birthday } = req.body
+
+  try {
+    const existingCustomer = await db.query(
+      'SELECT * FROM customers WHERE id = $1',
+      [id]
+    )
+
+    if (existingCustomer.rows.length === 0) {
+      return res.status(404).json({ error: 'Cliente não encontrado' })
+    }
+
+    if (existingCustomer.rows[0].cpf !== cpf) {
+      const cpfExists = await db.query(
+        'SELECT * FROM customers WHERE cpf = $1',
+        [cpf]
+      )
+      if (cpfExists.rows.length > 0) {
+        return res.status(409).send('O CPF já está associado a outro cliente.')
+      }
+    }
+
+    const updateQuery =
+      'UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE id = $5 RETURNING *'
+    const values = [name, phone, cpf, birthday, id]
+
+    const result = await db.query(updateQuery, values)
+
+    res.status(200).json(result.rows[0])
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
+}
--- src/routes/customers.routes.js
+++ src/routes/customers.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { validateSchema } from '../middlewares/validateSchema.middleware.js'
-import { getCustomers, getCustomersById, postCustomers } from '../controllers/customers.controller.js'
+import { editCustomerById, getCustomers, getCustomersById, postCustomers } from '../controllers/customers.controller.js'
 import { customersSchema } from '../schemas/customers.schema.js'
 
 const customersRouter = Router()
@@ -8,8 +8,8 @@ const customersRouter = Router()
 customersRouter.get('/customers', getCustomers)
 customersRouter.post('/customers', validateSchema(customersSchema), postCustomers)
 customersRouter.get("/customers/:id", getCustomersById)
+customersRouter.put("/customers/:id", validateSchema(customersSchema), editCustomerById)
 // receitasRouter.post("/receitas", validateSchema(receitaSchema), createReceita)
 // receitasRouter.delete("/receitas/:id", deleteReceita)
-// receitasRouter.put("/receitas/:id", validateSchema(receitaSchema), editReceitaById)
 
 export default customersRouter
